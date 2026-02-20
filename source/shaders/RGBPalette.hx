de099cbef8ac26a02146282aa3e8aab984719939
Andre Nicholas Jr <pumpgaming2@gmail.com>
2026-02-20 15:36:00 -0600

    i think rgb shader will compile if so then ready for 0.0.2

--- source/shaders/RGBPalette.hx
+++ source/shaders/RGBPalette.hx
@@ -1,158 +1,131 @@
 package shaders;
-import objects.Note;
+
+import flixel.FlxG;
 import flixel.FlxSprite;
-import flixel.util.FlxMath;
-import flixel.graphics.shader.FlxShader;
+import flixel.math.FlxMath;
+import openfl.display.Shader;
+import openfl.display.ShaderFilter;
 
+/**
+ * RGBPalette shader data
+ */
 class RGBPalette {
-	public var shader(default, null):RGBPaletteShader = new RGBPaletteShader();
-	public var r(default, set):FlxColor;
-	public var g(default, set):FlxColor;
-	public var b(default, set):FlxColor;
-	public var mult(default, set):Float;
+    public var r(default, set):FlxColor;
+    public var g(default, set):FlxColor;
+    public var b(default, set):FlxColor;
+    public var mult(default, set):Float;
 
-	private function set_r(color:FlxColor) {
-		r = color;
-		shader.r.value = [color.redFloat, color.greenFloat, color.blueFloat];
-		return color;
-	}
+    public var shader:Shader;
 
-	private function set_g(color:FlxColor) {
-		g = color;
-		shader.g.value = [color.redFloat, color.greenFloat, color.blueFloat];
-		return color;
-	}
+    public function new() {
+        r = 0xFFFF0000; // red
+        g = 0xFF00FF00; // green
+        b = 0xFF0000FF; // blue
+        mult = 1.0;
 
-	private function set_b(color:FlxColor) {
-		b = color;
-		shader.b.value = [color.redFloat, color.greenFloat, color.blueFloat];
-		return color;
-	}
-	
-	private function set_mult(value:Float) {
-		mult = FlxMath.bound(value, 0, 1);
-		shader.mult.value = [mult];
-		return mult;
-	}
+        shader = new Shader();
+        updateShader();
+    }
 
-	public function new()
-	{
-		r = 0xFFFF0000;
-		g = 0xFF00FF00;
-		b = 0xFF0000FF;
-		mult = 1.0;
-	}
+    private function updateShader():Void {
+        shader.data.r = { value: [r.redFloat, r.greenFloat, r.blueFloat] };
+        shader.data.g = { value: [g.redFloat, g.greenFloat, g.blueFloat] };
+        shader.data.b = { value: [b.redFloat, b.greenFloat, b.blueFloat] };
+        shader.data.mult = { value: [FlxMath.bound(mult, 0, 1)] };
+    }
+
+    private function set_r(value:FlxColor):FlxColor {
+        r = value;
+        updateShader();
+        return r;
+    }
+
+    private function set_g(value:FlxColor):FlxColor {
+        g = value;
+        updateShader();
+        return g;
+    }
+
+    private function set_b(value:FlxColor):FlxColor {
+        b = value;
+        updateShader();
+        return b;
+    }
+
+    private function set_mult(value:Float):Float {
+        mult = FlxMath.bound(value, 0, 1);
+        updateShader();
+        return mult;
+    }
 }
 
-// automatic handler for easy usability
-class RGBShaderReference
-{
-	public var r(default, set):FlxColor;
-	public var g(default, set):FlxColor;
-	public var b(default, set):FlxColor;
-	public var mult(default, set):Float;
-	public var enabled(default, set):Bool = true;
+/**
+ * Reference to a sprite's RGB shader
+ * Allows toggling on/off globally or per-sprite
+ */
+class RGBShaderReference {
+    public var r(default, set):FlxColor;
+    public var g(default, set):FlxColor;
+    public var b(default, set):FlxColor;
+    public var mult(default, set):Float;
+    public var enabled(default, set):Bool;
 
-	public var parent:RGBPalette;
-	private var _owner:FlxSprite;
-	private var _original:RGBPalette;
-	public function new(owner:FlxSprite, ref:RGBPalette)
-	{
-		parent = ref;
-		_owner = owner;
-		_original = ref;
-		owner.shader = ref.shader;
+    private var _owner:FlxSprite;
+    private var _palette:RGBPalette;
 
-		@:bypassAccessor
-		{
-			r = parent.r;
-			g = parent.g;
-			b = parent.b;
-			mult = parent.mult;
-		}
-	}
-	
-	private function set_r(value:FlxColor)
-	{
-		if(allowNew && value != _original.r) cloneOriginal();
-		return (r = parent.r = value);
-	}
-	private function set_g(value:FlxColor)
-	{
-		if(allowNew && value != _original.g) cloneOriginal();
-		return (g = parent.g = value);
-	}
-	private function set_b(value:FlxColor)
-	{
-		if(allowNew && value != _original.b) cloneOriginal();
-		return (b = parent.b = value);
-	}
-	private function set_mult(value:Float)
-	{
-		if(allowNew && value != _original.mult) cloneOriginal();
-		return (mult = parent.mult = value);
-	}
-	private function set_enabled(value:Bool)
-	{
-		_owner.shader = value ? parent.shader : null;
-		return (enabled = value);
-	}
+    /** Global list of all shader refs */
+    public static var instances:Array<RGBShaderReference> = [];
 
-	public var allowNew = true;
-	private function cloneOriginal()
-	{
-		if(allowNew)
-		{
-			allowNew = false;
-			if(_original != parent) return;
+    public function new(owner:FlxSprite, palette:RGBPalette) {
+        _owner = owner;
+        _palette = palette;
 
-			parent = new RGBPalette();
-			parent.r = _original.r;
-			parent.g = _original.g;
-			parent.b = _original.b;
-			parent.mult = _original.mult;
-			_owner.shader = parent.shader;
-			//trace('created new shader');
-		}
-	}
-}
+        r = palette.r;
+        g = palette.g;
+        b = palette.b;
+        mult = palette.mult;
+        enabled = true;
 
-class RGBPaletteShader extends FlxShader {
-	@:glFragmentHeader('
-		#pragma header
-		
-		uniform vec3 r;
-		uniform vec3 g;
-		uniform vec3 b;
-		uniform float mult;
+        // apply shader to sprite
+        _owner.shader = palette.shader;
 
-		vec4 flixel_texture2DCustom(sampler2D bitmap, vec2 coord) {
-			vec4 color = flixel_texture2D(bitmap, coord);
-			if (!hasTransform || color.a == 0.0 || mult == 0.0) {
-				return color;
-			}
+        instances.push(this);
+    }
 
-			vec4 newColor = color;
-			newColor.rgb = min(color.r * r + color.g * g + color.b * b, vec3(1.0));
-			newColor.a = color.a;
-			
-			color = mix(color, newColor, mult);
-			
-			if(color.a > 0.0) {
-				return vec4(color.rgb, color.a);
-			}
-			return vec4(0.0, 0.0, 0.0, 0.0);
-		}')
+    public function set_r(value:FlxColor):FlxColor {
+        r = value;
+        _palette.r = value;
+        return r;
+    }
 
-	@:glFragmentSource('
-		#pragma header
+    public function set_g(value:FlxColor):FlxColor {
+        g = value;
+        _palette.g = value;
+        return g;
+    }
 
-		void main() {
-			gl_FragColor = flixel_texture2DCustom(bitmap, openfl_TextureCoordv);
-		}')
+    public function set_b(value:FlxColor):FlxColor {
+        b = value;
+        _palette.b = value;
+        return b;
+    }
 
-	public function new()
-	{
-		super();
-	}
+    public function set_mult(value:Float):Float {
+        mult = FlxMath.bound(value, 0, 1);
+        _palette.mult = mult;
+        return mult;
+    }
+
+    public function set_enabled(value:Bool):Bool {
+        enabled = value;
+        _owner.shader = value ? _palette.shader : null;
+        return enabled;
+    }
+
+    /** Toggle all shaders globally */
+    public static function setGlobalEnabled(value:Bool):Void {
+        for (ref in instances) {
+            ref.enabled = value;
+        }
+    }
 }
